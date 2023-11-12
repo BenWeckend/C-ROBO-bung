556a8ab4892e475774042929f86bd52b0329da75
Ben Weckend <ben.weckend@web.de>
2023-11-12 21:44:44 +0100

    first small tests using classes and objects in 1Test -> main.cpp

--- 1Tests/main.cpp
+++ 1Tests/main.cpp
@@ -1,14 +1,53 @@
 #include <iostream>
 #include <string>
+using euro = int;
 
-using namespace std;
+class bankaccount {
+public:
+	euro balance = 0;
+	std::string name;
+	std::string accountname;
+
+	int balanceChange(euro bchange) {
+		return calculate(bchange);
+	}
+
+	int balanceAmmount() {
+		return balance;
+	}
+
+
+private:
+	int calculate(euro money) {
+		balance += money;
+		
+		return balance;
+	}
+};
+
 int main() {
-	
-	short int age = 12;
-	
-	cout << age;
-	 
+	char choice;
+	int num = 0;
+	euro amount;
+	bankaccount mainName;
 
+	do {
+		std::cout << "\ntransfer money (type t)\nview account balance (type v)\nexit (type x)\nChoice: ";
+		std::cin >> choice; std::cout << std::endl;
+
+		switch (choice) {
+		case 't':
+			std::cout << "Amount: "; std::cin >> amount;
+			std::cout << "New balance: " << mainName.balanceChange(amount) << " euro" << std::endl;
+			break;
+		case 'v':
+			std::cout << "Balance: " << mainName.balanceAmmount() << " euro" << std::endl;
+		case 'x': break;
+		default:
+			std::cout << "Please enter an valid input!" << std::endl;
+			break;
+		}
+	} while (choice != 'x' );
 	
 	return 0;
 }
