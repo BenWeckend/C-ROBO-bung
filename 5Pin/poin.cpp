57b4b89a413295219327856c834d01f17f9ece88
Ben Weckend <ben.weckend@web.de>
2023-11-22 15:52:15 +0100

    work on poin.cpp in 5

--- 5Pin/poin.cpp
+++ 5Pin/poin.cpp
@@ -2,16 +2,16 @@
 #include <cstdlib>
 #include <string>
 
+int* numbersToOrder;
 class data {
 private:
-    int size = 1;
-    int *numbers = (int*) malloc(sizeof(int) * size);
-    int *sortetNumbers = (int*) malloc(sizeof(int) * size);
-
-
+    int size = 2;
+    int currentValue, numberAmount = 0;
+    int* numbers;
+    int* sortetNumbers;
 
 public:
-    int currentValue = 0, numberAmount = 0;
+    
     data();
 
     void view() {
@@ -19,30 +19,46 @@ public:
             std::cout << "Num[" << i << "] = " << numbers[i] << "\n";
         }
     }
+
     void save() {
-        char buffer[sizeof(int)];
-        do {
-            std::cout << "Enter Number (enter c to cancel): "; std::cin >> currentValue;
-           
-            if (numberAmount > size) {
+        currentValue = 0;
+        while (currentValue != -1) {
+            std::cout << "Enter Number (enter -1 to cancel): "; std::cin >> currentValue;
+            //std::cout << "value: " << currentValue << std::endl;
+            if (numberAmount >= size) {
                 size += 2;
                 numbers = (int*)realloc(numbers, sizeof(int) * size);
+                std::cout << "Allocating new space!\n";
             }
             numbers[numberAmount] = currentValue;
-
-
-
-        } while (std::__cxx11::to_string(currentValue) != "c");
-       
+            (currentValue == -1) ? 0 : numberAmount++;
+        }
     }
+    void order() {
+        int orderchoice;
+        std::cout << "small to big (1) or big to small (2)"; std::cin << orderchoice;
+        if
+    }
+
+    void freeNum() {
+        int free(int* numbers);
+        int free(int* sortetNumbers);
+        std::cout << "Memory is free again ....\n";
+    }
+
 
 };
 
 data::data() {
-
+    numbers = (int*)malloc(sizeof(int) * size);
+    sortetNumbers = (int*)malloc(sizeof(int) * size);
+    numbersToOrder = &numbers;
 }
 
-void order() {
+void SmallToBig() {
+    
+}
+void BigToSmall() {
 
 }
 
@@ -50,19 +66,20 @@ int main (){
     char choice;
     data newdata;
 
-    std::cout << "What do you wnat to do?: \n - save date (type s)\n - view data (type v)\n - order data (type o)\n - exit (type c)\n Choice: ";
-    std::cin >> choice;
-
     do{
+    std::cout << "What do you want to do?: \n - save date (type s)\n - view data (type v)\n - order data (type o)\n - exit (type c)\n Choice: ";
+    std::cin >> choice; std::cout << "\n";
+
         switch (choice)
         {
         case 's': newdata.save(); break;
         case 'v': newdata.view(); break;
-        case 'o': order(); break;
-        case 'c': break;
+        case 'o': newdata.order(); break;
+        case 'c': newdata.freeNum(); break;
         default: std::cerr << "wrong input!" << std::endl; break;
         }
     } while (choice != 'c');
 
+    
     return 0;
 }
